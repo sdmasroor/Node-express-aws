679397aee2ef04722ea97817f8b9766b4fdfd8a6
SD Masroor <syedmasroorjan@outlook.com>
2023-11-21 19:35:25 +0530

    'commit'

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3001;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World!\n');
+app.get('/', (req, res) => {
+  res.send('Hello, this is a simple API!');
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
 });
