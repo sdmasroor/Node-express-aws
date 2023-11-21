7fecc580765294bee72d5873da0e5ed2e9b087ed
SD Masroor <syedmasroorjan@outlook.com>
2023-11-21 21:59:12 +0530

    'first-commit'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello, this is a simple API!');
 });
 
